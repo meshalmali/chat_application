0232bc684e5dffcf5a7c24577710bb76c6fb2fd6
meshalmali <shalmalib15@gmail.com>
2022-09-30 08:50:46 +0530

    Remove Chat Scroll bar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -48,11 +48,13 @@ function Sidebar() {
           <input placeholder="Search a chat" type="text"></input>
         </div>
       </div>
-      <div className="sidebar_chats">
-        <SidebarChat addNewChat />
-        {rooms.map((room) => (
-          <SidebarChat key={room.id} id={room.id} name={room.data.name} />
-        ))}
+      <div className="sidebar_chats_parent">
+        <div className="sidebar_chats">
+          <SidebarChat addNewChat />
+          {rooms.map((room) => (
+            <SidebarChat key={room.id} id={room.id} name={room.data.name} />
+          ))}
+        </div>
       </div>
     </div>
   );
